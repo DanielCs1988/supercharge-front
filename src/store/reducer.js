ae264bbf0d402a45a417630d863c1d439119cf7c
DanielCs88 <daniel.cs20@gmail.com>
2018-09-10 10:55:17 +0200

    Only 2 cards can be images up.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -9,12 +9,43 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     if (action.type === CARD_CLICKED) {
-        return {
-            ...state,
-            cards: state.cards.map(card => card.id === action.id ? {...card, flipped: !card.flipped} : card)
-        }
+        return flipCard(state, action);
     }
     return state;
+};
+
+const flipCard = (state, action) => {
+    switch (state.flippedCards.length) {
+        case 0:
+            return {
+                ...state,
+                cards: state.cards.map(card => card.id === action.id ? {...card, flipped: true} : card),
+                flippedCards: [action.id]
+            };
+        case 1:
+            return {
+                ...state,
+                cards: state.cards.map(card => card.id === action.id ? {...card, flipped: true} : card),
+                flippedCards: [...state.flippedCards, action.id]
+            };
+        case 2:
+            const newCards = state.cards.map(card => {
+                if (card.flipped) {
+                    return {...card, flipped: false};
+                }
+                if (card.id === action.id) {
+                    return {...card, flipped: true}
+                }
+                return card;
+            });
+            return {
+                ...state,
+                cards: newCards,
+                flippedCards: [action.id]
+            };
+        default:
+            return state;
+    }
 };
 
 export default reducer;
